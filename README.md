9d0d4cff9e8cd5d5e2ce217ef7d5491190b1eab0
cskksc <ckoparkar@live.in>
2015-03-02 19:57:25 +0530

    GET / runs 'SELECT ok'

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # postgresql-smoketest
 CF app to test database connectivity.
+
+## Usage
+
+This sinatra app executes `SELECT 'ok'` for every `GET /` request.
+It assumes that the postgresql service instance is bound to this app, and has the following details:
+* username: Database username.
+* password: Password for the user `username`.
+* db_name: Name of the database.
+* port: Port on which postgresql is running.
--- postgresql_smoketest.rb
+++ postgresql_smoketest.rb
@@ -1,7 +1,22 @@
 require 'sinatra/base'
+require 'json'
+require 'pg'
 
 class PostgresqlSmoketest < Sinatra::Base
   get '/' do
-    "hello world"
+    connection.exec("SELECT 'ok'").values.first.first
+  end
+
+  private
+
+  def credentials
+    JSON.parse(ENV['VCAP_SERVICES'])['postgresql-db'].first['credentials']
+  end
+
+  def connection
+    @conn ||= PG.connect(host: credentials['hostname'],
+                        user: credentials['username'],
+                        port: credentials['port'],
+                        dbname: credentials['db_name'])
   end
 end
